4d3256f92ff1b80972f0be97fb9cc68b8fb90c69
João Pedro Vitor de Oliveira <joao.pvo@aluno.ufop.edu.br>
2022-01-21 02:39:23 -0300

    get na rota tweets com bug de clonar mensagens

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 const users = [];
 const tweets = [];
+const posts = [];
+let post = { username: "", avatar: "", tweet: "" };
 
 app.post("/sign-up", (req, res) => {
   const user = req.body;
@@ -16,8 +18,26 @@ app.post("/sign-up", (req, res) => {
 
 app.post("/tweets", (req, res) => {
   const published = req.body;
-  tweets.push(published.tweet);
+  tweets.push(published);
   res.send("OK");
+  console.log(tweets);
+  console.log(users);
+
+  for (let i = 0; i < tweets.length; i++) {
+    for (let j = 0; j < users.length; j++) {
+      if (tweets[i].username === users[j].username) {
+        post.username = users[j].username;
+        post.avatar = users[j].avatar;
+        post.tweet = tweets[i].tweet;
+        posts.push(post);
+      }
+    }
+  }
+});
+
+app.get("/tweets", (req, res) => {
+    console.log(posts)
+  res.send(posts);
 });
 
 app.listen(5000);
