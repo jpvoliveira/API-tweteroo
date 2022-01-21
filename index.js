f56ecf1bff7dc3a656cf50ca12c894087aff1f35
João Pedro Vitor de Oliveira <joao.pvo@aluno.ufop.edu.br>
2022-01-21 01:59:46 -0300

    post para a rota tweets concluido

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors())
+app.use(express.json());
+
 const users = [];
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
   const user = req.body;
   users.push(user);
+  res.send("OK");
+});
+
+app.post("/tweets", (req, res) => {
+  const published = req.body;
+  tweets.push(published.tweet);
   res.send("OK");
 });
 
